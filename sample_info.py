c7e3e4bec884bdd89bf1d5340b392de211c5d747
Bookworm2024 <infohubnetworks@gmail.com>
2024-09-08 12:28:35 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 21145186
+API_HASH = 'daa53f4216112ad22b8a8f6299936a46'
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [6011680723]
+CHANNELS = [-1002265877051]
 AUTH_USERS = []
-AUTH_CHANNEL = None
+AUTH_CHANNEL = [-1002123546604]
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Telegram'
+DATABASE_URI = "mongodb+srv://infohubstore06:OG9Gbh5PKiXu2uwN@cluster2454.5llyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2454"
+DATABASE_NAME = 'Cluster2454'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
